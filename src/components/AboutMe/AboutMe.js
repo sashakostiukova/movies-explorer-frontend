bec35aec06e86e3cc61648cb655306ad8ca7273e
sashakostiukova <sashakostiukova@yandex.ru>
2024-05-17 01:03:42 +0300

    update README, AboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './AboutMe.css';
 import SectionHeader from '../SectionHeader/SectionHeader';
-import studentPhoto from '../../images/portfolio-img.png';
+import studentPhoto from '../../images/portfolio-img.jpg';
 
 export default function AboutMe() {
   return (
@@ -12,10 +12,12 @@ export default function AboutMe() {
       <div className="about-me__container">
 
         <div className="about-me__information">
-          <h3 className="about-me__name">Виталий</h3>
-          <p className="about-me__description">Фронтенд-разработчик, 30 лет</p>
-          <p className="about-me__text">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена 
-            и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+          <h3 className="about-me__name">Александра</h3>
+          <p className="about-me__description">Фронтенд-разработчица, 30 лет</p>
+          <p className="about-me__text">Люблю воплощать идеи в реальные проекты.
+Благодаря нестандартному опыту работы в НКО, не боюсь и умею устанавливать коммуникацию с разными людьми. Ценю возможность быть частью команды и вкладывать силы в общий проект.
+Веб-разработка привлекла меня своим простором для творчества и понятными инструментами. Мне нравится осваивать новое и расширять свою компетенцию.
+Хочу развиваться в этом направлении.</p>
           <a className="about-me__link link-transition" target="_blank" rel="noopener noreferrer" href="https://github.com/sashakostiukova">Github</a>
         </div>
 
